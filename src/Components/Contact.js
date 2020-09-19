6bc22209ce4a8e5b32f788c1020fed4156f2124d
FrostTusk <frosttusk@gmail.com>
2020-09-20 01:12:42 +0200

    Added entire font-awesome

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -88,24 +88,18 @@ class Contact extends Component {
 				   </div>
 
                <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
+                  <h4 className="widget-title">Preferred Contact Method</h4>
                   <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
+                    <li>
+                      <span>
+                        Career: LinkedIn
+                      </span>
+                    </li>
+                    <li>
+                      <span>
+                        Stream: Twitch/Twitter
+                      </span>
+                    </li>
                   </ul>
 		         </div>
             </aside>
